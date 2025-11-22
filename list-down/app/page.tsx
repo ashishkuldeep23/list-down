771eabf73d369510137abbaaac3792577977c320
Ashish kuldeep <ashishkuldeep6@gmail.com>
2025-11-22 22:58:26 +0530

    Sending files on gitHub

---
+++ list-down/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className=" min-h-screen ">
+
+      <p>Move to bottom</p>
+      <p>Move to bottom</p>
+      <p>Move to bottom</p>
+
+      
+    </div>
+  );
+}
